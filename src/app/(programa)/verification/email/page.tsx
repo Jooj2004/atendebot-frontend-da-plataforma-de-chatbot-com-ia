861d4e4ce5ee8c67401dd97ae540647bbae52bca
Josdegar <josdegarfsantos@gmail.com>
2025-08-05 13:25:03 -0300

    start page verification email

--- src/app/(programa)/verification/email/page.tsx
+++ src/app/(programa)/verification/email/page.tsx
@@ -1,6 +1,30 @@
+"use client"
+import { useCompanyStore } from "@/store/company"
+
 const EmailVerify = () => {
+    const company = useCompanyStore()
     return(
-        <div>...</div>
+        <div className="py-3 text-white px-8 min-h-screen bg-gradient-to-br from-[#2825eb] via-[#1915eb] to-[#5403d6]">
+            <header>
+                Logo e botão Sair que vai para tela de login
+            </header>
+            <div>
+                <div>
+                    <h1>Confirme seu cadastro</h1>
+                    <p>Insira o código enviado para {company.company?.email || '*não há email da company ainda*'} </p>
+                </div>
+                <div>
+                    <div className="border border-white/40 rounded-md">
+                        Caixa para digitar os numeros
+                    </div>
+                </div>
+                <div>
+                    Botão de CONFIRMAR
+                    <p>Não receber o código? <span className="hover:underline cursor-pointer">Reenviar</span></p>
+                        Veja como fazer o hover funcionar no reenviar mesmo colocando o mouse no p. voce viu isso nas aulas de tailwind
+                </div>
+            </div>
+        </div>
     )
 }
 
