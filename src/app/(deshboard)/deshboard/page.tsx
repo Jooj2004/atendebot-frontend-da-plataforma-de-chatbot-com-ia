e476edd4f05dc4b0141d26237993735c3033ba3c
Josdegar <josdegarfsantos@gmail.com>
2025-08-16 17:42:37 -0300

    init faq page

--- src/app/(deshboard)/deshboard/page.tsx
+++ src/app/(deshboard)/deshboard/page.tsx
@@ -70,7 +70,7 @@ const Deshboard = () => {
                 <div className="flex-1 ml-[24%] bg-secondary px-1">
                     <div>
                         <h1 className="text-lg mb-1 font-semibold">Deshboard</h1>
-                        <p className="text-[7px] text-cyan-700 mb-2">Resumo das atividades e informações principais</p>
+                        <p className="text-[7px] md:text-[10px] text-cyan-700 mb-2">Resumo das atividades e informações principais</p>
                     </div>
                     <div>
                         <p className="p-2 text-xs text-cyan-950 border-1 border-b-gray-900/50 rounded-md bg-white"> {`${process.env.NEXT_PUBLIC_SERVER_URL}/chat/new/${company.company?.id}`} </p>
--- src/app/(deshboard)/faqs/page.tsx
+++ src/app/(deshboard)/faqs/page.tsx
@@ -1,8 +1,66 @@
+"use client"
+
+import { Button } from "@/components/ui/button"
+import { useTokenStore } from "@/store/token"
+import { useFaqs } from "@/utils/queries"
+
 const Faqs = () => {
+    const token = useTokenStore()
+
+    const faqs = useFaqs(token.token as string)
+
+    const handleEdit = (companyId: string) => {
+
+    }
+
+    const handleExclud = (companyId: string) => {
+
+    }
+
     return(
-        <div className="flex-1 ml-[24%] bg-secondary">
-            faqs
-        </div>
+        <>
+            {faqs.isLoading &&
+                <>Carregando...</>
+            }
+            {!faqs.isLoading &&
+                <div className="flex-1 ml-[24%] bg-secondary px-1">
+                    <div>
+                        <h1 className="text-lg mb-1 font-semibold">FAQs</h1>
+                        <p className="text-[7px] md:text-[10px] text-cyan-700 mb-2">Cadastre as perguntas mais frequentes dos clientes. O chatbot usará essas informações para responder automaticamente. Dica: Priorize clareza e relevância!</p>
+                    </div>
+                    <div className="mx-auto bg-red-500 w-60 h-70 md:w-1/2">
+                        Tabela para criar novas faqs
+                    </div>
+                    {faqs.data && faqs.data.length > 0 &&
+                        <div>
+                            <h2 className="font-semibold md:text-lg">Suas FAQs</h2>
+                            {faqs.data.map((e,index) => (
+                                <div key={index} className="border flex my-3 flex-col gap-2 mx-1 p-1 md:px-2 md:py-3 rounded-md shadow-md">
+                                    <p className="text-sm md:text-md font-semibold text-blue-950">{e.question}</p>
+                                    <p className="text-xs md:text-sm text-cyan-900">{e.answer}</p>
+                                    <div>
+                                        <Button 
+                                            className="mr-2" 
+                                            variant="destructive" 
+                                            size="sm"
+                                            onClick={() => handleEdit(e.companyId as string)}
+                                        >Editar</Button>
+
+                                        <Button 
+                                            size="sm"
+                                            onClick={() => handleExclud(e.companyId as string)}
+                                        >Excluir</Button>
+                                    </div>
+                                </div>
+                            ))}
+                        </div>
+                    }
+                    {faqs.data && faqs.data.length <= 0 &&
+                        <></>
+                    }
+                </div>
+            }
+        </>
     )
 }
 
